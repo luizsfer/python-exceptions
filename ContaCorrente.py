d97a57df503ffa4e587b1c59aab07fc37af1d140
Luiz Ferreira <luiz.ferreira@outlook.com>
2019-09-08 14:17:15 -0300

    Aula 1

--- ContaCorrente.py
+++ ContaCorrente.py
@@ -1,11 +1,14 @@
 """ByteBank - Tratamento de excessões com Python."""
+import logging
 
 
 class ContaCorrente:
     """Criação de um objeto do tipo ContaCorrente."""
 
     _totalDeContasCriadas = 0
-    saldo = 100
+    _taxaDeOperacao = 0
+    _saldo = 100
+    _cliente = ''
 
     def sacar(self, valor):
         """Função que realiza saques em Conta Corrente."""
@@ -28,9 +31,31 @@ class ContaCorrente:
             contaDestino.depositar(valor)
             return True
 
-    def __init__(self, numero, agencia, Cliente):
+    def __init__(self, numero, agencia):
         """Inicializador do objeto."""
         self.numero = numero
         self.agencia = agencia
-        self.Cliente = Cliente
+        ContaCorrente._taxaDeOperacao = (
+            30 / ContaCorrente._totalDeContasCriadas
+            )
         ContaCorrente._totalDeContasCriadas += 1
+
+
+def dividir(numero, divisor):
+    """Função que realiza a divisão de dois números."""
+    conta = ContaCorrente(None)
+    print(conta._saldo)
+    return numero / divisor
+
+
+def testaDivisao(divisor):
+    """Função que executa a outra função chamada dividir."""
+    return dividir(10, divisor)
+
+
+def metodo():
+    """Função que chama a função testaDivisao."""
+    try:
+        testaDivisao(0)
+    except TypeError as e:
+        logging.exception(e)
