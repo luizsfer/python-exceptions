95c85b49b0dc5a864f544b94ae1f7108d9c75e92
Luiz Ferreira <luiz.ferreira@outlook.com>
2019-09-09 01:01:14 -0300

    Aula 5 - Observação: Python não possui polimorfismo de sobrecarga de método, portanto essa aula não obteve mudanças

---
+++ OperacaoFinanceiraException.py
@@ -0,0 +1,9 @@
+"""ByteBank - Tratamento de excessões com Python."""
+
+
+class OperacaoFinanceiraException (Exception):
+    """Classe de funções de tratamento de excessões."""
+
+    """Essa aula não pôde ser traduzida, pois o Python não
+    suporta polimorfismo de sobrecarga"""
+    pass
